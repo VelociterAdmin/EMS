170e0b0604182f3e635533ce21511b86f4641b19
Danish968 <qureshidanish3000@gmail.com>
2022-07-29 11:26:54 +0530

    committed Employee class

--- src/main/java/com/velociter/ems/pojo/Employee.java
+++ src/main/java/com/velociter/ems/pojo/Employee.java
@@ -1,5 +1,106 @@
 package com.velociter.ems.pojo;
 
 public class Employee {
+	private String firstName;
+	private String lastName;
+	private String employeeId;
+	private String employeeEmailId;
+	private long   mobileNumber;
+	private String employeeFatherName;
+	private String employeeMotherName;
+	private String designation;
+	private String userName;
+	private String Password;
 
-}
+	Address address = new Address();
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeEmailId() {
+		return employeeEmailId;
+	}
+
+	public void setEmployeeEmailId(String employeeEmailId) {
+		this.employeeEmailId = employeeEmailId;
+	}
+
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmployeeFatherName() {
+		return employeeFatherName;
+	}
+
+	public void setEmployeeFatherName(String employeeFatherName) {
+		this.employeeFatherName = employeeFatherName;
+	}
+
+	public String getEmployeeMotherName() {
+		return employeeMotherName;
+	}
+
+	public void setEmployeeMotherName(String employeeMotherName) {
+		this.employeeMotherName = employeeMotherName;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+
+	}
