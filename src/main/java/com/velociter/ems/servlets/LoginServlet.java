f5547a9d20178bf5c646a6eeefbc888b84f2f1ff
ShwetaMiashra09 <shwetamishra09082000@gmail.com>
2022-07-30 18:48:14 +0530

    added LoginServlet

--- src/main/java/com/velociter/ems/servlets/LoginServlet.java
+++ src/main/java/com/velociter/ems/servlets/LoginServlet.java
@@ -1,11 +1,17 @@
 package com.velociter.ems.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.velociter.ems.pojo.Employee;
+import com.velociter.ems.pojo.LoginValidation;
 
 /**
  * Servlet implementation class LoginServlet
@@ -25,8 +31,28 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter writer=response.getWriter();
+		//writer.print("<h1>Welcome to register </h1>");
+		Employee employee= new Employee();
+		
+		employee.setUsername(request.getParameter("Username"));
+		employee.setPassword(request.getParameter("Password"));
+		
+	
+		if(LoginValidation.validate(employee.getUsername(), employee.getPassword())) {
+			writer.print("Success");
+            RequestDispatcher rd=request.getRequestDispatcher("Welcome.jsp");
+            rd.forward(request, response);
+		}
+		else {
+			writer.print("failed");
+			 RequestDispatcher rd=request.getRequestDispatcher("Login.jsp");
+	            rd.include(request, response);
+
+		}
 	}
 
 }
