fdb1fca24fdad3a2ad712800a0846b1e9ae685da
ShwetaMiashra09 <shwetamishra09082000@gmail.com>
2022-07-30 18:55:18 +0530

    added WelcomeServlet

---
+++ src/main/java/com/velociter/ems/servlets/WelcomeServlet.java
@@ -0,0 +1,71 @@
+package com.velociter.ems.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.velociter.ems.pojo.Employee;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		Employee emp = new Employee();
+	try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
+		PreparedStatement ps=con.prepareStatement("select * from emp where empname=? and city=?");
+		ps.setString(1,emp.getUsername());
+		ps.setString(2,emp.getPassword());
+		ResultSet rs= ps.executeQuery();
+		while(rs.next()) {
+			pw.println("Username="+ rs.getString(1));
+			pw.println("Password="+ rs.getString(2));
+		}
+	}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
