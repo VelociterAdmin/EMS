4268a63b22fa53328db8fdc3aab57a0f61ae828f
Shafique98 <mohammed.shafiq.899@gmail.com>
2022-08-08 10:21:26 +0530

    added oroject files

--- src/main/java/com/velociter/ems/servlets/WelcomeServlet.java
+++ src/main/java/com/velociter/ems/servlets/WelcomeServlet.java
@@ -35,11 +35,11 @@ public class WelcomeServlet extends HttpServlet {
 	 *      response)
 	 */
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//			throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
